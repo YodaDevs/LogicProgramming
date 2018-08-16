a225d1b8ce83d9f30d120460190ed099464ddf06
YodaDevs <37128180+YodaDevs@users.noreply.github.com>
2018-08-15 23:17:33 -0300

    Update family.pl

--- family.pl
+++ family.pl
@@ -1,21 +1,29 @@
+% Fatos.
+
 mãe(marcia,marcus).
 mãe(marcia,mary).
 mãe(marcia,meiry).
 mãe(marcia,neide).
+
 mãe(neide,mateus).
 mãe(neide,joão).
+
 pai(mauro,marcus).
 pai(mauro,mary).
 pai(mauro,meiry).
 pai(mauro,neide).
+
 mulher(marcia).
 mulher(neide).
 mulher(meiry).
 mulher(mary).
+
 homem(mauro).
 homem(marcus).
 homem(mateus).
 homem(joão).
+
+% Regras
 avô(X, Y) :- pai(X, Z), pai(Z, Y); pai(X, Z), mãe(Z,Y).
 avó(X, Y) :- mãe(X, Z), mãe(Z, Y); mãe(X, Z), pai(Z,Y).
 irmao(X,Y) :- mãe(Z,X), mãe(Z,Y), homem(X); pai(Z,X), mãe(Z,Y), homem(X).
